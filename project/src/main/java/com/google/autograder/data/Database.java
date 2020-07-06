73f6508e0039ae8b837028e154576866f66b6aee
Mahima Shanware <mahima.shanware@gmail.com>
2020-07-06 19:21:49 +0000

    Add addAssignment and addQuestion without uniqueness check.

--- project/src/main/java/com/google/autograder/data/Database.java
+++ project/src/main/java/com/google/autograder/data/Database.java
@@ -1,19 +1,46 @@
 package com.google.autograder.data;
 
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+
 /** Class containing basic database functionalities. */
 public class Database {
     
+  //connect to database
   DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+  
   //TODO: add variables
 
-  //connect to database
-
   //store item in database (general)
+  private void putEntity(Entity entity) {
+      datastore.put(entity);
+  }
 
   //load classes
 
   //create operations for all entities (with pk and fk restraints)
-    //ensure new objects have diff main names
+  //FOR FUTURE: ensure new objects have diff main names
+
+  public void addAssignment(int totalPoints) {
+    Entity assignmentEntity = new Entity("Assignment");
+    assignmentEntity.setProperty("totalPoints", totalPoints);
+    assignmentEntity.setProperty("status", "SAMPLE_PENDING");
+    datastore.put(assignmentEntity);
+  }
+
+  public void addQuestion(Entity assignmentEntity, String questionName, String questionType, int questionPoints) {
+      Entity questionEntity = new Entity("Question");
+      questionEntity.setProperty("name", questionName);
+      questionEntity.setProperty("type", questionType);
+      questionEntity.setProperty("points", questionPoints);
+      questionEntity.setProperty("assignmentID", assignmentEntity.id());
+      datastore.put(questionEntity);
+  }
+
 
   //get all assignments
 
