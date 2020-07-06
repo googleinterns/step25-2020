263eb4db3aa4dddd425da4ec11bdfa27e016dddf
Mahima Shanware <mahima.shanware@gmail.com>
2020-07-06 18:45:19 +0000

    Update pom.xml, comments for database functions.

---
+++ project/src/main/java/com/google/autograder/data/Database.java
@@ -0,0 +1,31 @@
+package com.google.autograder.data;
+
+/** Class containing basic database functionalities. */
+public class Database {
+    
+  DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+  //TODO: add variables
+
+  //connect to database
+
+  //store item in database (general)
+
+  //load classes
+
+  //create operations for all entities (with pk and fk restraints)
+    //ensure new objects have diff main names
+
+  //get all assignments
+
+  //get all questions for an assignment
+
+  //get all submissions for an assignment
+
+  //get all answers for a question
+
+  //set/edit location for a question
+
+  //get all groups for a question
+
+  //change answer's group
+}
