9a7ab033f070528cc1f0e53893f4a7ecaaef0073
Ben Lin <blin1@umd.edu>
2020-07-06 23:17:40 +0000

    storing coordinates on dataservlet

---
+++ project/src/main/java/com/google/autograder/servlets/manageBoxServlet.java
@@ -0,0 +1,80 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.servlets;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson; 
+import com.google.gson.GsonBuilder;  
+import java.util.HashMap;
+import java.util.Map;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/manageBox")
+public class manageBoxServlet extends HttpServlet {
+  
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {     
+    response.setContentType("application/json");
+
+// next 3 lines are broken
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Query query = new Query("Coordinates");
+    Entity entity = datastore.prepare(query);
+
+    String lx = (String) entity.getProperty("lx");
+    String ly = (String) entity.getProperty("ly");
+    String rx = (String) entity.getProperty("ry");
+    String ry = (String) entity.getProperty("ry");
+
+    String coordinates = "lx: "+lx+"ly:"+ly+"rx:"+rx+"ry:"+ry;
+    
+    String json = new Gson().toJson(coordinates);
+    response.getWriter().println(json);
+
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    String lx = request.getParameter("lx");
+    String ly = request.getParameter("ly");
+    String rx = request.getParameter("rx");
+    String ry = request.getParameter("ry");
+
+    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+    Entity messageEntity = new Entity("Coordinates");
+    messageEntity.setProperty("lx", lx);
+    messageEntity.setProperty("ly", ly);
+    messageEntity.setProperty("rx", rx);
+    messageEntity.setProperty("ry", ry);
+
+    datastore.put(messageEntity);
+  }
+
+}
