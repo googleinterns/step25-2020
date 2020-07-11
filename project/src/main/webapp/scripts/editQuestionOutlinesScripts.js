291cd4cdf426aac6f9ab38bbbccb57e220a67817
Rodney Sampson II <rsampdev@gmail.com>
2020-07-11 00:38:52 +0000

    Import Ben's Front End Styling

--- project/src/main/webapp/scripts/editQuestionOutlinesScripts.js
+++ project/src/main/webapp/scripts/editQuestionOutlinesScripts.js
@@ -88,7 +88,6 @@ function nextButton() {
 }
 
 
-
 function mousePositions() {
 
   function getMousePos(canvas, evt) {
@@ -107,13 +106,11 @@ function mousePositions() {
     var x = mousePos.x.toFixed(0);
     var y = mousePos.y.toFixed(0);
     writePos(x, y);
-    // var message = 'Corner Position: ' + mousePos.x.toFixed(1) + ',  ' + mousePos.y.toFixed(1);
-    // writePos(message);
-
   }, false);
 
   let corner = "left"; // variable to alternate which corner's location is being registered
   var lx, ly, rx, ry;
+  var questionName, questionPoints;
   function writePos(x, y) {
     var upperLeft = document.getElementById('upperLeft');
     var lowerRight = document.getElementById('lowerRight');
@@ -124,20 +121,26 @@ function mousePositions() {
       ly = y;
       upperLeft.innerHTML = "Upper Left Corner: " + x + ", " + y;
       corner = "right";
-      setCorner.innerHTML = "Click Lower Right corner"
+      setCorner.innerHTML = "Click Lower Right corner";
     } else {
       rx = x;
       ry = y;
       lowerRight.innerHTML = "Lower Right Corner: " + x + ", " + y;
       corner = "left";
-      setCorner.innerHTML = "Click Upper Left corner if it's inaccurate"
+      setCorner.innerHTML = "Click Upper Left corner if it's inaccurate";
       crop(lx, ly, rx, ry);
     }
 
   }
+
+  // listener to send post request with coordinates for datastore
+  var submitCoordinates = document.getElementById('submit');
+  submitCoordinates.addEventListener('click', function(evt) {
+    var url = '/manageBox?lx='+lx+'&ly='+ly+'&rx='+rx+'&ry='+ry; 
+    fetch(url, {method:"POST"});
+  });
 }
 
-// still need to scale the cropped canvas
 function crop(lx, ly, rx, ry) {
   var cropX = lx;
   var cropY = ly;
@@ -152,145 +155,15 @@ function crop(lx, ly, rx, ry) {
   cropCanvas = document.getElementById("cropCanvas").getContext('2d');
 
   cropCanvas.drawImage(pdfCanvas, cropX, cropY, cropWidth, cropHeight, 0, 0, cropWidth, cropHeight);
-
 }
 
 
-// function drawRect(canvas) {
-//     function setMousePosition(e) {
-//         var ev = e || window.event; //Moz || IE
-//         if (ev.pageX) { //Moz
-//             mouse.x = ev.pageX + window.pageXOffset;
-//             mouse.y = ev.pageY + window.pageYOffset;
-//         } else if (ev.clientX) { //IE
-//             mouse.x = ev.clientX + document.body.scrollLeft;
-//             mouse.y = ev.clientY + document.body.scrollTop;
-//         }
-//     };
+// function cropAllPDFs() {
+//     fetch('/manageBox').then(response => response.json()).then(data => {
 
-//     var mouse = {
-//         x: 0,
-//         y: 0,
-//         startX: 0,
-//         startY: 0
-//     };
-//     var element = null;
+//         console.log(data);
 
-//     canvas.onmousemove = function (e) {
-//         setMousePosition(e);
-//         if (element !== null) {
-//             element.style.width = Math.abs(mouse.x - mouse.startX) + 'px';
-//             element.style.height = Math.abs(mouse.y - mouse.startY) + 'px';
-//             element.style.left = (mouse.x - mouse.startX < 0) ? mouse.x + 'px' : mouse.startX + 'px';
-//             element.style.top = (mouse.y - mouse.startY < 0) ? mouse.y + 'px' : mouse.startY + 'px';
-//         }
-//     }
+//         // crop all the pdfs based off the coordinates
+//     });
 
-//     canvas.onclick = function (e) {
-//         if (element !== null) {
-//             element = null;
-//             canvas.style.cursor = "default";
-//             console.log("finsihed.");
-//         } else {
-//             console.log("begun.");
-//             mouse.startX = mouse.x;
-//             mouse.startY = mouse.y;
-//             element = document.createElement('div');
-//             element.className = 'rectangle'
-//             element.style.left = mouse.x + 'px';
-//             element.style.top = mouse.y + 'px';
-//             canvas.appendChild(element)
-//             canvas.style.cursor = "crosshair";
-//         }
-//     }
-// }
-
-
-
-// function notworking() {
-//   var canvas = document.getElementById("canvas");
-// var ctx = canvas.getContext("2d");
-
-// var canvasOffset = $("#canvas").offset();
-// var offsetX = canvasOffset.left;
-// var offsetY = canvasOffset.top;
-
-// var isDrawing = false;
-// var startX;
-// var startY;
-
-//     mouseX = parseInt(e.clientX - offsetX);
-//     mouseY = parseInt(e.clientY - offsetY);
-//     $("#downlog").html("Down: " + mouseX + " / " + mouseY);
-
-//     // Put your mousedown stuff here
-//     if (isDrawing) {
-//         isDrawing = false;
-//         ctx.beginPath();
-//         ctx.rect(startX, startY, mouseX - startX, mouseY - startY);
-//         ctx.fill();
-//         canvas.style.cursor = "default";
-//     } else {
-//         isDrawing = true;
-//         startX = mouseX;
-//         startY = mouseY;
-//         canvas.style.cursor = "crosshair";
-//     }
-// }
-
-// $("#canvas").mousedown(function (e) {
-//     handleMouseDown(e);
-// });
-
-
-
-// initDraw(document.getElementById('canvas'));
-
-// function initDraw(canvas) {
-//     function setMousePosition(e) {
-//         var ev = e || window.event; //Moz || IE
-//         if (ev.pageX) { //Moz
-//             mouse.x = ev.pageX + window.pageXOffset;
-//             mouse.y = ev.pageY + window.pageYOffset;
-//         } else if (ev.clientX) { //IE
-//             mouse.x = ev.clientX + document.body.scrollLeft;
-//             mouse.y = ev.clientY + document.body.scrollTop;
-//         }
-//     };
-
-//     var mouse = {
-//         x: 0,
-//         y: 0,
-//         startX: 0,
-//         startY: 0
-//     };
-//     var element = null;
-
-//     canvas.onmousemove = function (e) {
-//         setMousePosition(e);
-//         if (element !== null) {
-//             element.style.width = Math.abs(mouse.x - mouse.startX) + 'px';
-//             element.style.height = Math.abs(mouse.y - mouse.startY) + 'px';
-//             element.style.left = (mouse.x - mouse.startX < 0) ? mouse.x + 'px' : mouse.startX + 'px';
-//             element.style.top = (mouse.y - mouse.startY < 0) ? mouse.y + 'px' : mouse.startY + 'px';
-//         }
-//     }
-
-//     canvas.onclick = function (e) {
-//         if (element !== null) {
-//             element = null;
-//             canvas.style.cursor = "default";
-//             console.log("finsihed.");
-//         } else {
-//             console.log("begun.");
-//             mouse.startX = mouse.x;
-//             mouse.startY = mouse.y;
-//             element = document.createElement('div');
-//             element.className = 'rectangle'
-//             element.style.left = mouse.x + 'px';
-//             element.style.top = mouse.y + 'px';
-//             canvas.appendChild(element)
-//             canvas.style.cursor = "crosshair";
-//         }
-//     }
 // }
