3aee95c3ca70a8f64aef46d2c9072460f8bba3f9
Rodney Sampson II <rsamp@google.com>
2020-07-24 04:47:45 +0000

    Clean up Client.java to remove servlet testing

--- project/src/main/java/com/google/autograder/servlets/helpers/Client.java
+++ project/src/main/java/com/google/autograder/servlets/helpers/Client.java
@@ -1,34 +1,19 @@
 package com.google.autograder.servlets.helpers;
 
 import java.io.File;
-import java.net.URL;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import org.json.simple.JSONObject;
-import javax.servlet.http.HttpServlet;
 import org.json.simple.parser.JSONParser;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/client")
-public final class Client extends HttpServlet {
+public final class Client {
 
     public static final String CLIENT_ID = getClientID();
     public static final String CLIENT_SECRET = getClientSecret();
 
     private static final String RESOURCES_PATH = "../../../project/src/main/resources";
     private static final String CREDENTIALS_PATH = "/auth/credentials.json";
-
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String credentials = null;
-
-        credentials = "{ \"credentials\" : { \"client_id\": \"" + CLIENT_ID + "\" , \"client_secret\": \"" + CLIENT_SECRET + "\" }}";
-
-        response.getWriter().println(credentials);
-    }
 
     private static String getClientID() {
         String client_id = null;
