9749989b9cb5c190a68f3196f134b1f7a8e77840
Ben Lin <blin1@umd.edu>
2020-07-31 04:29:04 +0000

    brush up icons

--- project/src/main/webapp/scripts/assignment.js
+++ project/src/main/webapp/scripts/assignment.js
@@ -16,30 +16,18 @@ async function getQuestionFunctions() {
 
     var myDiv = document.getElementById('question-functions');
 
-    // var editOutline = document.createElement('a');
     var editOutline = document.getElementById('editOutline');
-    // editOutline.innerHTML = "Edit Question Outlines<br><br>";
     editOutline.href = `/pages/editOutline.html${assignmentParameters}`;
 
-    // var viewSubmissions = document.createElement('a');
     var viewSubmissions = document.getElementById('viewSubmissions');
-    // viewSubmissions.innerHTML = "View Submissions<br><br>";
     viewSubmissions.href = `/pages/viewSubmissions.html${assignmentParameters}`;
 
-    // var gradeSubmissions = document.createElement('a');
     var gradeSubmissions = document.getElementById('gradeSubmissions');
-    // gradeSubmissions.innerHTML = "Grade Submissions<br><br>";
     gradeSubmissions.href = `/pages/gradeSubmissions.html${assignmentParameters}`;
 
-    // var reviewGrades = document.createElement('a');
     var reviewGrades = document.getElementById('reviewGrades');
-    // reviewGrades.innerHTML = "Review Grades<br><br>";
     reviewGrades.href = `/pages/reviewGrades.html${assignmentParameters}`;
 
-    // myDiv.appendChild(editOutline);
-    // myDiv.appendChild(viewSubmissions);
-    // myDiv.appendChild(gradeSubmissions);
-    // myDiv.appendChild(reviewGrades);
 }
 
 getQuestionFunctions();
