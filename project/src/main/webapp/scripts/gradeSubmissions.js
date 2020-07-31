946d1ab72065d0ddfad6d1939892f7fcdf096b40
Ben Lin <blin1@umd.edu>
2020-07-31 22:52:41 +0000

    grading page frontend

--- project/src/main/webapp/scripts/gradeSubmissions.js
+++ project/src/main/webapp/scripts/gradeSubmissions.js
@@ -7,49 +7,16 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-async function getQuestionFunctions() {
-    const urlParameters = new URLSearchParams(window.location.search);
-    const assignmentID = urlParameters.get("assignmentID");
-    const courseID = urlParameters.get("courseID");
-    const assignmentKey = urlParameters.get("assignment-key");
-    
-    const assignmentParameters = `?courseID=${courseID}&assignmentID=${assignmentID}&assignment-key=${assignmentKey}`;
-
-    var editOutline = document.getElementById('editOutline');
-    editOutline.href = `/pages/editOutline.html${assignmentParameters}`;
-
-    var viewSubmissions = document.getElementById('viewSubmissions');
-    viewSubmissions.href = `/pages/viewSubmissions.html${assignmentParameters}`;
-
-    var gradeSubmissions = document.getElementById('gradeSubmissions');
-    gradeSubmissions.href = `/pages/gradeSubmissions.html${assignmentParameters}`;
-
-    var reviewGrades = document.getElementById('reviewGrades');
-    reviewGrades.href = `/pages/reviewGrades.html${assignmentParameters}`;
-}
-
-getQuestionFunctions();
-
-
+getOptions();
 async function getOptions() {
     const assignmentKey = getUrlVars()['assignment-key'];                                                                                                                                                                       
     var myDiv = document.getElementById('grading-options');
 
-    var gradeIndivid = document.createElement('a');
-    gradeIndivid.innerHTML = "Grade Individual Submissions<br><br>";
+    var gradeIndivid = document.getElementById("gradeIndividualSubmissions");
     gradeIndivid.href = "/pages/gradeIndividualSubmissions.html?assignment-key=" + assignmentKey;
 
-    var questionGroups = document.createElement('a');
-    questionGroups.innerHTML = "Grade Question Groups<br><br>";
+    var questionGroups = document.getElementById("questionGroups");
     questionGroups.href = "/pages/questionPage.html?assignment-key=" + assignmentKey;
-
-    var editOutline = document.createElement('a');
-    editOutline.innerHTML = "Review Question Outlines<br><br>";
-    editOutline.href = "/pages/editOutline.html?assignment-key=" + assignmentKey;
-
-    myDiv.appendChild(gradeIndivid);
-    myDiv.appendChild(questionGroups);
-    myDiv.appendChild(editOutline);
 }
 
 // https://html-online.com/articles/get-url-parameters-javascript/
