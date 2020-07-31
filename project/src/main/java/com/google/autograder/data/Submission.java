2fb514afa94bf074032a76bdfbe0bb7e7fda7fa5
Rodney Sampson II <rsamp@google.com>
2020-07-31 02:36:56 +0000

    Remove blobKey from Submission.java

--- project/src/main/java/com/google/autograder/data/Submission.java
+++ project/src/main/java/com/google/autograder/data/Submission.java
@@ -10,20 +10,18 @@ public final class Submission {
     public String key;
     public String graded;
     public String userID;
-    public String blobKey;
     public String courseID;
     public String assignmentID;
     public String submissionID;
     public String assignmentKey;
     public String driveFileLink;
 
-    public Submission(String key, String graded, String userID, String blobKey, String courseID, String assignmentID, String submissionID, String assignmentKey, String driveFileLink) {
+    public Submission(String key, String graded, String userID, String courseID, String assignmentID, String submissionID, String assignmentKey, String driveFileLink) {
         this.driveFileLink = driveFileLink;
         this.assignmentKey = assignmentKey;
         this.submissionID = submissionID;
         this.assignmentID = assignmentID;
         this.courseID = courseID;
-        this.blobKey = blobKey;
         this.userID = userID;
         this.graded = graded;
         this.key = key;
