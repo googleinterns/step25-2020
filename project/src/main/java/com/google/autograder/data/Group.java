bdfa73f92e428b8702d106c0da565c551f3be63d
Rodney Sampson II <rsamp@google.com>
2020-08-04 20:49:22 +0000

    Clean up Group.java

--- project/src/main/java/com/google/autograder/data/Group.java
+++ project/src/main/java/com/google/autograder/data/Group.java
@@ -5,23 +5,23 @@ import com.google.appengine.api.datastore.KeyFactory;
 
 /** Class containing group data. */
 public class Group {
-
-  public int score;
-  public String questionKey;
-  public String key;
-
-  public Group(int score, String questionKey, Key key) {
-    this.score = score;
-    this.questionKey = questionKey;
-    this.key = KeyFactory.keyToString(key);
-  }
-
-  public String getKeyString() {
-      return this.key;
-  }
-
-  public Key getKey() {
-      return KeyFactory.stringToKey(this.key);
-  }
-
+    
+    public int score;
+    public String key;
+    public String questionKey;
+    
+    public Group(int score, String questionKey, Key key) {
+        this.key = KeyFactory.keyToString(key);
+        this.questionKey = questionKey;
+        this.score = score;
+    }
+    
+    public String getKeyString() {
+        return this.key;
+    }
+    
+    public Key getKey() {
+        return KeyFactory.stringToKey(this.key);
+    }
+    
 }
