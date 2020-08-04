25fce6843075f6794a1b2069535be24a7a2f00b3
Rodney Sampson II <rsamp@google.com>
2020-08-04 21:27:02 +0000

    Clean up GroupServlet.java

--- project/src/main/java/com/google/autograder/servlets/GroupServlet.java
+++ project/src/main/java/com/google/autograder/servlets/GroupServlet.java
@@ -1,35 +1,33 @@
 package com.google.autograder.servlets;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Arrays;
-import com.google.autograder.data.Database;
-import com.google.autograder.data.Answer;
-import com.google.gson.Gson;
 import java.io.IOException;
-import javax.servlet.annotation.WebServlet;
+import java.util.ArrayList;
+import com.google.gson.Gson;
 import javax.servlet.http.HttpServlet;
+import com.google.autograder.data.Answer;
+import com.google.autograder.data.Database;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.PreparedQuery;
-import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.Query.SortDirection;
 
 public final class GroupServlet extends HttpServlet {
+    
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        // get all the answer entities based on answer + question keys
+        System.out.println("Creating groups...");
+        
+        String assignmentKey = request.getParameter("assignment-key");
+        String questionKey = request.getParameter("question-key");
+        
+        String json = Database.createGroups(assignmentKey, questionKey);
+        
+        System.out.println("Done!");
+        System.out.println(json);
+        
+        response.setContentType("application/json;");
+        response.getWriter().println(json);
+    }
 
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    // get all the answer entities based on answer + question keys
-    System.out.println("Creating groups...");
-    String assignmentKey = request.getParameter("assignment-key");
-    String questionKey = request.getParameter("question-key");
-
-    String json = Database.createGroups(assignmentKey, questionKey);
-    System.out.println("Done!");
-    System.out.println(json);
-    response.setContentType("application/json;");
-    response.getWriter().println(json);
-  }
-  
 }
