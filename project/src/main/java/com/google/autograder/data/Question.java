4e36f9f47ab1bf61772d30839b58c426b5ae140e
Rodney Sampson II <rsamp@google.com>
2020-08-04 20:51:15 +0000

    Clean up Question.java

--- project/src/main/java/com/google/autograder/data/Question.java
+++ project/src/main/java/com/google/autograder/data/Question.java
@@ -5,27 +5,27 @@ import com.google.appengine.api.datastore.KeyFactory;
 
 /** Class containing question data. */
 public class Question {
-
-  public String name;
-  public String type;
-  public int points;
-  public String assignmentKey;
-  public String key;
-
-  public Question(String name, int points, String type, String assignmentKey, Key key) {
-    this.name = name;
-    this.type = type;
-    this.points = points;
-    this.assignmentKey = assignmentKey;
-    this.key = KeyFactory.keyToString(key);
-  }
-
-  public String getKeyString() {
-      return this.key;
-  }
-
-  public Key getKey() {
-      return KeyFactory.stringToKey(this.key);
-  }
-
+    
+    public int points;
+    public String key;
+    public String name;
+    public String type;
+    public String assignmentKey;
+    
+    public Question(String name, int points, String type, String assignmentKey, Key key) {
+        this.key = KeyFactory.keyToString(key);
+        this.assignmentKey = assignmentKey;
+        this.points = points;
+        this.name = name;
+        this.type = type;
+    }
+    
+    public String getKeyString() {
+        return this.key;
+    }
+    
+    public Key getKey() {
+        return KeyFactory.stringToKey(this.key);
+    }
+    
 }
