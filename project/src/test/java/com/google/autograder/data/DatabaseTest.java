cef32b0f195afe06f7f7008715c33d65fdb5ac42
Ben Lin <blin1@umd.edu>
2020-08-06 21:34:17 +0000

    addMultipleAnswers Test

--- project/src/test/java/com/google/autograder/data/DatabaseTest.java
+++ project/src/test/java/com/google/autograder/data/DatabaseTest.java
@@ -14,7 +14,8 @@ import org.junit.runners.JUnit4;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Logger;
+import java.util.List;
+import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -58,21 +59,6 @@ public final class DatabaseTest {
   public void tearDown() {
       helper.tearDown();
   }
-
-  /**
-   * Tests method getCoursesData
-   */
-//   @Test
-//   public void getCoursesDataTest() {
-//     LocalUserServiceTestConfig userServiceConfig = new LocalUserServiceTestConfig();
-//     userServiceConfig.setOAuthEmail("myemail@email.com");
-//     userServiceConfig.setOAuthEmail("user1");
-
-//     String actualCoursesData = Database.getCoursesData();
-//     String expectedCoursesData = "badString";
-
-//     Assert.assertEquals(actualCoursesData, expectedCoursesData);
-//   }
 
   @Test
   public void saveASingleEntity() {
@@ -128,6 +114,34 @@ public final class DatabaseTest {
     Assert.assertEquals(questionKey, "questionKey1");
   }
 
+ /**
+  * Test addAnswer Method with multiple answers
+  */
+  @Test
+  public void addMultipleAnswersTest() {
+    Database.addAnswer("filePath1", "parsedAnswer1", 5, "assignmentKey1", "questionKey1");
+    Database.addAnswer("filePath2", "parsedAnswer3", 6, "assignmentKey1", "questionKey1");
+    Database.addAnswer("filePath3", "parsedAnswer2", 7, "assignmentKey1", "questionKey1");
+    Database.addAnswer("filePath4", "parsedAnswer5", 8, "assignmentKey1", "questionKey1");
+    Database.addAnswer("filePath5", "parsedAnswer4", 9, "assignmentKey1", "questionKey1");
+    Iterable<Entity> query = Database.query(new Query("Answer"));
 
+    List<String> expectedAnswers = new ArrayList<>();
+    expectedAnswers.add("parsedAnswer1");
+    expectedAnswers.add("parsedAnswer3");
+    expectedAnswers.add("parsedAnswer2");
+    expectedAnswers.add("parsedAnswer5");
+    expectedAnswers.add("parsedAnswer4");
+
+    int count = 0;
+    List<String> actualAnswers = new ArrayList<>();
+    for (Entity entity : query) {
+        count++;
+        actualAnswers.add(entity.getProperty("parsedAnswer").toString());
+    }
+
+    Assert.assertEquals(expectedAnswers, actualAnswers);
+    
+  }
 
 }
